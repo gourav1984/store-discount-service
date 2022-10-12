01031aeff1e1e87fbf2d30f018d9be6d0c1928cd
GAURAV <gourav.tanpure@gmail.com>
2022-10-12 22:29:16 +0530

    Store API ...

---
+++ src/main/java/com/retailstore/discounts/domain/User.java
@@ -0,0 +1,38 @@
+package com.retailstore.discounts.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.List;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private List<Role> roles;
+
+    @NotNull
+    private Instant createdAt;
+
+}
